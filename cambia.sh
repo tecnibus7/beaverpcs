da867fbfa556b3d6dcbc4d9f6168c514981b3865
Jesus <tecnibus7@us.es>
2018-09-17 13:04:49 +0200

    Actualizacion del fichero de inicio de elementos de usuario

--- cambia.sh
+++ cambia.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+
 # chown biblioteca /home/usuario/.config/lxsession/Lubuntu/autostart
-echo "bash /etc/bus/desktop-config/cambia-fondo.sh" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
-echo "python /etc/bus/beaverpcs/pangolinDice/pangolinDice" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
-echo "bash /etc/bus/apagado/apaga-equipo.sh" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
+## echo "bash /etc/bus/desktop-config/cambia-fondo.sh" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
+## echo "python /etc/bus/beaverpcs/pangolinDice/pangolinDice" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
+## echo "bash /etc/bus/apagado/apaga-equipo.sh" | tee -a /home/usuario/.config/lxsession/Lubuntu/autostart
 # chown usuario /home/usuario/.config/lxsession/Lubuntu/autostart
+
+bash /etc/bus/desktop-config/cambia-fondo.sh
+bash /etc/bus/desktop-config/cambia-fondo.sh
+python /etc/bus/beaverpcs/pangolinDice/pangolinDice
+bash /etc/bus/apagado/apaga-equipo.sh
