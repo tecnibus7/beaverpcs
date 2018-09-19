f369d84cad52dce47813664315dd1bb072a0bde9
Jesus <tecnibus7@us.es>
2018-09-19 13:47:49 +0200

    Anadiendo copia de iconos escritorio

--- cambia.sh
+++ cambia.sh
@@ -2,3 +2,4 @@ bash /etc/bus/desktop-config/cambia-fondo.sh
 bash /etc/bus/desktop-config/cambia-fondo.sh
 python /etc/bus/beaverpcs/pangolinDice/pangolinDice
 bash /etc/bus/apagado/apaga-equipo.sh
+cp -R /etc/bus/beaverpcs/usuario/soft/usuario/Desktop/* /home/usuario/Desktop
