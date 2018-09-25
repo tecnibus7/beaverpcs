548c384b089d98e51fa57e76bf031c0fb85eb57a
Jesus <tecnibus7@us.es>
2018-09-25 10:55:56 +0200

    Arreglado autoconfiguracion de firefox y configuracion para DesktopEstritorio

--- configura-firefox.sh
+++ configura-firefox.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-/bin/cd /home/usuario/.mozilla/firefox/*.default/
-/bin/cp -R /etc/bus/beaverpcs/soft/firefox/prefs.js prefs.js
-/bin/cd ~
+
+cd /home/usuario/.mozilla/firefox/
+DIRECCION=$(find /home/usuario/.mozilla/firefox/ | grep prefs.js)
+# DIRECCION=$(readlink -f prefs.js)
+# FICHERO="prefs.js"
+cd ~
+echo $DIRECCION
+cp -R /etc/bus/beaverpcs/soft/firefox/prefs.js $DIRECCION
+
+
+## /bin/cd /home/usuario/.mozilla/firefox/*.default/
+## /bin/cp -R /etc/bus/beaverpcs/soft/firefox/prefs.js prefs.js
+## /bin/cd ~
--- configura-iconos.sh
+++ configura-iconos.sh
@@ -3,3 +3,7 @@ cp -R /etc/bus/beaverpcs/soft/usuario/Desktop/* /home/usuario/Escritorio
 cd /home/usuario/Escritorio
 ln -s /home/usuario/Documentos
 cd ~
+cp -R /etc/bus/beaverpcs/soft/usuario/Desktop/* /home/usuario/Desktop
+cd /home/usuario/Desktop
+ln -s /home/usuario/Documentos
+cd ~
