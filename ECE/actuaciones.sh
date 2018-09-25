dcc7c43456b8897fe09947279a10edc05d291c93
Jesus <tecnibus7@us.es>
2018-09-25 13:14:29 +0200

    Agregando actualización de repositorios a ECE

--- ECE/actuaciones.sh
+++ ECE/actuaciones.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+### Actualiza los repositorios ###
+cd /etc/bus/beaverpcs
+/usr/bin/git fetch origin
+cd ~
+### Fin de la actualización de repositorios ###
+
+### Añadir aquí lo que se quiera ejecutar,   ###
+### y se ejecutará en el plazo de dos horas. ###
+### crontab ejecutará como administrador     ###
