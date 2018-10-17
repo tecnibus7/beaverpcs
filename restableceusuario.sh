fa9ae81d2cd2867ef6e86b2122fa310a72a13693
Jesus <tecnibus7@us.es>
2018-10-17 10:04:40 +0200

    Corrigiendo errotes

--- restableceusuario.sh
+++ restableceusuario.sh
@@ -4,7 +4,7 @@
 
 /usr/sbin/userdel -r usuario
 /usr/sbin/useradd -m -g users -s /bin/bash usuario
-/bin/echo "usuario:usuario" | chpasswd
+/bin/echo "usuario:usuario" | /usr/sbin/chpasswd
 # echo -e "autologin-user=usuario" | tee -a /usr/share/lightdm/lightdm.conf.d/20-lubuntu.conf
 # echo -e "autologin-user-timeout=0" | tee -a /usr/share/lightdm/lightdm.conf.d/20-lubuntu.conf
 # echo -e "numlock=1" | tee -a /usr/share/lightdm/lightdm.conf.d/20-lubuntu.conf
