6e1bbaecdb54cedbb4c6dfc941dcdf64d6dbdff1
Jesus <tecnibus7@us.es>
2018-10-30 10:39:20 +0100

    Modificando fichero de automontado

--- espacio_usuario/se_usuario.sh
+++ espacio_usuario/se_usuario.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-/bin/umount /mnt/fs_usuario_test
-losetup -d /dev/loop1
+# /bin/umount /mnt/fs_usuario_test
+# losetup -d /dev/loop1
+# losetup /dev/loop1 usuario_fs
+# /bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
+
+dd if=/dev/zero of=usuario_fs bs=1024 count=1072000
+# Cargando en el loop0 el fichero creado
 losetup /dev/loop1 usuario_fs
-/bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
+# Damos formato al ficheros
+mkfs -t ext3 -m 1 -v /dev/loop1
+# Montar el fichero en una ubicación
+mkdir /mnt/fs_usuario_test
+mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
+# Creo un enlace al espacio de ficheros creados
+# cambio los permisos para el fichero para permitir escritura al usuario_fs
+chown -R usuario /mnt/fs_usuario_test
+chmod -R +rw /mnt/fs_usuario_test
+chown usuario usuario_fs
+# ln -s /mnt/fs_usuario_test /home/usuario/Documentos_u_test
+## chown -R usuario /home/usuario/Documentos_u
+
+#
+echo "= Espacio de trabajo montado ="
