c00287313cd7ebbd685d1fcb520568253cff2838
Jesus <tecnibus7@us.es>
2018-10-31 12:40:24 +0100

    Corrigiendo errores de permisos en el punto de montaje

--- espacio_usuario/se_usuario.sh
+++ espacio_usuario/se_usuario.sh
@@ -4,8 +4,8 @@
 ##losetup /dev/loop1 usuario_fs
 ##/bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
 
-chown -R usuario /mnt/fs_usuario
-chmod -R +rw /mnt/fs_usuario
+chown -R usuario /mnt/fs_usuario/
+chmod -R +rw /mnt/fs_usuario/
 chown usuario usuario_fs
 chattr -i /home/usuario/Desktop/
 chown usuario /home/usuario/Desktop/Documentos
@@ -15,7 +15,7 @@ chattr -i /home/usuario/Escritorio/
 chown usuario /home/usuario/Escritorio/Documentos
 chattr +i /home/usuario/Escritorio/
 
-chown usuario /mnt/fs_usuario/
+# chown usuario /mnt/fs_usuario/
 
 # ln -s /mnt/fs_usuario_test /home/usuario/Documentos_u_test
 ## chown -R usuario /home/usuario/Documentos_u
