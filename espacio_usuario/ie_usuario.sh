0749795309d880d5d54bf61871293486b802dfc3
Jesus <tecnibus7@us.es>
2018-11-02 12:33:59 +0100

    Cambiando rutas

--- espacio_usuario/ie_usuario.sh
+++ espacio_usuario/ie_usuario.sh
@@ -15,7 +15,7 @@ mount -t ext3 /dev/loop1 /mnt/fs_usuario/
 # cambio los permisos para el fichero para permitir escritura al usuario_fs
 chown usuario /mnt/fs_usuario/
 chmod -R +rw /mnt/fs_usuario/
-chown usuario usuario_fs
+chown usuario /usuario_fs
 
 mv /home/usuario/Descargas /mnt/fs_usuario
 mv /home/usuario/Imágenes /mnt/fs_usuario
