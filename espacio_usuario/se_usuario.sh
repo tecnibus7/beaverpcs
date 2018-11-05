d9c168f4c8bab57af563cade75f03e383bd87587
Jesus <tecnibus7@us.es>
2018-11-05 13:29:40 +0100

    Cambiando orden de permisos y creacion de ficheros

--- espacio_usuario/se_usuario.sh
+++ espacio_usuario/se_usuario.sh
@@ -3,23 +3,22 @@
 ##losetup -d /dev/loop1
 ##losetup /dev/loop1 usuario_fs
 ##/bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
-cd /
+zzsleep 40
 /bin/chown usuario /usuario_fs
-/bin/chown -R usuario /mnt/fs_usuario
-/bin/chmod -R +rw /mnt/fs_usuario
+/bin/chown usuario /mnt/fs_usuario/
+/bin/chmod -R +rw /mnt/fs_usuario/
+
 /usr/bin/chattr -i /home/usuario/Desktop/
+/bin/mkdir /mnt/fs_/home/usuario/Desktop/Documentos
 /bin/chown usuario /home/usuario/Desktop/Documentos
 /usr/bin/chattr +i /home/usuario/Desktop/
 
 /usr/bin/chattr -i /home/usuario/Escritorio/
+/bin/mkdir /mnt/fs_/home/usuario/Escritorio/Documentos
 /bin/chown usuario /home/usuario/Escritorio/Documentos
 /usr/bin/chattr +i /home/usuario/Escritorio/
 
 /bin/mkdir /mnt/fs_usuario/Descargas
-/bin/chown -R usuario /mnt/fs_usuario/Descargas
-/bin/chmod -R +rw /mnt/fs_usuario/Descargas
-
-
 #mv /home/usuario/Descargas /mnt/fs_usuario
 #mv /home/usuario/Imágenes /mnt/fs_usuario/
 #mv /home/usuario/Música /mnt/fs_usuario/
