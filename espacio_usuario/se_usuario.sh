a6a4117a8266274c7215393ac399b1186a550c0c
Jesus <tecnibus7@us.es>
2018-11-05 09:29:51 +0100

    Retrasando se_usuario para dar permisos y crear directorios

--- espacio_usuario/se_usuario.sh
+++ espacio_usuario/se_usuario.sh
@@ -3,7 +3,7 @@
 ##losetup -d /dev/loop1
 ##losetup /dev/loop1 usuario_fs
 ##/bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
-
+sleep 40
 /bin/chown usuario /mnt/fs_usuario/
 /bin/chmod -R +rw /mnt/fs_usuario/
 /bin/chown usuario /usuario_fs
@@ -15,15 +15,15 @@
 /bin/chown usuario /home/usuario/Escritorio/Documentos
 /usr/bin/chattr +i /home/usuario/Escritorio/
 
-mkdir /mnt/fs_usuario/Descargas
-mv /home/usuario/Descargas /mnt/fs_usuario
-mv /home/usuario/Imágenes /mnt/fs_usuario/
-mv /home/usuario/Música /mnt/fs_usuario/
-mv /home/usuario/Plantillas /mnt/fs_usuario
-mv /home/usuario/Imágenes /mnt/fs_usuario
-mv /home/usuario/Videos /mnt/fs_usuario
-rm /home/usuario/Público
-rm /home/usuario/Documentos
+/bin/mkdir /mnt/fs_usuario/Descargas
+#mv /home/usuario/Descargas /mnt/fs_usuario
+#mv /home/usuario/Imágenes /mnt/fs_usuario/
+#mv /home/usuario/Música /mnt/fs_usuario/
+#mv /home/usuario/Plantillas /mnt/fs_usuario
+#mv /home/usuario/Imágenes /mnt/fs_usuario
+#mv /home/usuario/Videos /mnt/fs_usuario
+#rm /home/usuario/Público
+#rm /home/usuario/Documentos
 
 
 # chown usuario /mnt/fs_usuario/
