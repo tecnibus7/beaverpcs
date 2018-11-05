751cbfcc1ce56a389d34613264be4875a070672c
Jesus <tecnibus7@us.es>
2018-11-05 13:34:49 +0100

    Corrigiento zzzzsleep en fichero se_usuario.sh

--- espacio_usuario/se_usuario.sh
+++ espacio_usuario/se_usuario.sh
@@ -3,7 +3,7 @@
 ##losetup -d /dev/loop1
 ##losetup /dev/loop1 usuario_fs
 ##/bin/mount -t ext3 /dev/loop1 /mnt/fs_usuario_test/
-zzsleep 40
+
 /bin/chown usuario /usuario_fs
 /bin/chown usuario /mnt/fs_usuario/
 /bin/chmod -R +rw /mnt/fs_usuario/
