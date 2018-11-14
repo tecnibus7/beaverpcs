e9ae16763653f3c5d059dbca18050c4d6d5e856d
Jesus <tecnibus7@us.es>
2018-11-14 13:26:04 +0100

    Estableciendo display

--- apagado/en-mantenimiento.sh
+++ apagado/en-mantenimiento.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export DISPLAY=0
 zenity --info --text "El equipo entrará en mantenimiento en 5 minutos, por favor, guarde sus trabajos. Disculpe las molestias. Servicio de Informática"
 /sbin/shutdown -r +5
