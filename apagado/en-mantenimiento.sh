fe6c22883d4b4e51d013cb3191c2476fce9b3676
Jesus <tecnibus7@us.es>
2018-11-14 13:31:49 +0100

    Ruta completa

--- apagado/en-mantenimiento.sh
+++ apagado/en-mantenimiento.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISPLAY=0
-zenity --info --text "El equipo entrará en mantenimiento en 5 minutos, por favor, guarde sus trabajos. Disculpe las molestias. Servicio de Informática"
+/usr/bin/zenity --info --text "El equipo entrará en mantenimiento en 5 minutos, por favor, guarde sus trabajos. Disculpe las molestias. Servicio de Informática"
 /sbin/shutdown -r +5
