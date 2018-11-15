4939cde6f189a8049677660664925980ddaa7b98
Jesus <tecnibus7@us.es>
2018-11-15 12:18:29 +0100

    Ultimo apagado

--- apagado/notifica-apagado.sh
+++ apagado/notifica-apagado.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-zenity --info --text "Le recordamos que el horario del servicio finaliza a las 21 horas. Guarde sus trabajos para no perderlos."
+/usr/bin/zenity --info --text "Le recordamos que el horario del servicio finaliza a las 21 horas. Guarde sus trabajos para no perderlos."
