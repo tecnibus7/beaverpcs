e4b25288e095bd5e3f03a43ed1e14c4ea53880a1
Jesus <tecnibus7@us.es>
2018-11-15 11:16:18 +0100

    Notificacion de en mantenimiento

--- apagado/en-mantenimiento.sh
+++ apagado/en-mantenimiento.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DISPLAY=0
+# export DISPLAY=0
 /usr/bin/zenity --info --text "El equipo entrará en mantenimiento en 5 minutos, por favor, guarde sus trabajos. Disculpe las molestias. Servicio de Informática"
 /sbin/shutdown -r +5
