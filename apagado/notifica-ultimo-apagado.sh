cfe2b75a99d003656a3376874d41f213bd862607
Jesus <tecnibus7@us.es>
2018-11-15 12:15:32 +0100

    Modificando script notificacion apagado

--- apagado/notifica-ultimo-apagado.sh
+++ apagado/notifica-ultimo-apagado.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-zenity --info --text "El horario del servicio finalizará en 5 minutos, y el equipo se apagará de forma automática. Guarde sus trabajos para no perderlos.\n Esta es la ultima notificación que se mostrará."
+/usr/bin/zenity --info --text "El horario del servicio finalizará en 5 minutos, y el equipo se apagará de forma automática. Guarde sus trabajos para no perderlos.\n Esta es la ultima notificación que se mostrará."
