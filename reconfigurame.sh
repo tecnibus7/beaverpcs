7560bf0e5fdebbbf86e39b1a84240002972bfae1
Jesus <tecnibus7@us.es>
2018-11-26 10:20:56 +0100

    Cambiando metodo de desactivacion de IPv6, funciona Cambios a ser confirmados: 	modificado:     reconfigurame.sh

--- reconfigurame.sh
+++ reconfigurame.sh
@@ -72,18 +72,20 @@ echo "= Contraseña para usuario desactivada ="
 ## echo "Usuario con clave"
 echo "= Desactivando IPv6 ="
 
-sed -i 's/net.ipv6.conf.all.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
-sed -i 's/net.ipv6.conf.default.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
-sed -i 's/net.ipv6.conf.lo.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 quiet splash"//g' "/etc/default/grub"
 
-echo -e "net.ipv6.conf.all.disable_ipv6 = 1" | tee -a /etc/sysctl.d/99-sysctl.conf
-echo -e "net.ipv6.conf.default.disable_ipv6 = 1" | tee -a /etc/sysctl.d/99-sysctl.conf
-echo -e "net.ipv6.conf.lo.disable_ipv6 = 1" | tee -a /etc/sysctl.d/99-sysctl.conf
-sysctl -p
-sleep 2
-echo "¿IPv6 desactivado? (true=1)"
-cat /proc/sys/net/ipv6/conf/all/disable_ipv6
-sleep 10
+# sed -i 's/net.ipv6.conf.all.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
+# sed -i 's/net.ipv6.conf.default.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
+# sed -i 's/net.ipv6.conf.lo.disable_ipv6 = 1//g' "/etc/sysctl.d/99-sysctl.conf"
+
+echo -e 'GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 quiet splash"' | tee -a /etc/default/grub
+# echo -e "net.ipv6.conf.default.disable_ipv6 = 1" | tee -a /etc/sysctl.d/99-sysctl.conf
+# echo -e "net.ipv6.conf.lo.disable_ipv6 = 1" | tee -a /etc/sysctl.d/99-sysctl.conf
+# sysctl -p
+# sleep 2
+# echo "¿IPv6 desactivado? (true=1)"
+# cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+# sleep 10
 echo "= Desactivado IPv6 ="
 
 echo "= Desactivando SSH para no administradores ="
