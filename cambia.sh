07d56bc22d909e0c137f4499587847121cddc6a3
Jesus <tecnibus7@us.es>
2018-11-27 14:03:00 +0100

    Añadido a inicio de sesion limpieza de espacio de trabajo y papelera

--- cambia.sh
+++ cambia.sh
@@ -4,3 +4,4 @@ bash /etc/bus/configura-lo.sh
 bash /etc/bus/screensavers/lanza-xscreensaver.sh
 bash /etc/bus/apagado/limpia-bash_history.sh
 bash /etc/bus/apagado/apaga-equipo.sh
+bash /etc/bus/restauraespacio.sh
