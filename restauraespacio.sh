dbcf8d3204f0876ecde44c738d057e3709404cda
Jesus <tecnibus7@us.es>
2018-11-27 14:01:29 +0100

    Preparando fichero para limpieza de espacio de usuario automatica

--- restauraespacio.sh
+++ restauraespacio.sh
@@ -15,11 +15,17 @@
 #fi
 #########################################
 
-/usr/bin/find /home/usuario/Descargas/* -mtime +3 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Escritorio/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Documentos/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Imágenes/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Música/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Plantillas/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Público/* -mtime +7 -exec /bin/rm {} \;
-/usr/bin/find /home/usuario/Vídeos/* -mtime +7 -exec /bin/rm {} \;
+/usr/bin/find /mnt/fs_usuario/Descargas/* -mtime +2 -exec /bin/rm {} \;
+/usr/bin/find /mnt/fs_usuario/* -mtime +2 -exec /bin/rm {} \;
+/bin/mkdir -p /mnt/fs_usuario/Descargas
+
+/usr/bin/find /home/usuario/.local/share/Trash/files/* -mtime +1 -exec /bin/rm {} \;
+
+#/usr/bin/find /home/usuario/Descargas/* -mtime +3 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Escritorio/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Documentos/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Imágenes/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Música/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Plantillas/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Público/* -mtime +7 -exec /bin/rm {} \;
+#/usr/bin/find /home/usuario/Vídeos/* -mtime +7 -exec /bin/rm {} \;
