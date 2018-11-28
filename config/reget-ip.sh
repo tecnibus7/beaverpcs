da10be1cf42534cd159348dbf9891bbd4a58a411
Jesus <tecnibus7@us.es>
2018-11-28 09:29:15 +0100

    Test para reobtener IP

--- config/reget-ip.sh
+++ config/reget-ip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /sbin/dhclient -r
+/bin/chown root /home/usuario/.reget-ip.txt
 /home/usuario/.reget-ip.txt</sbin/dhclient -v
+/bin/chown usuario /home/usuario/.reget-ip.txt
 # /usr/bin/touch /home/bibliotecario/ejecuta.txt
