c6b63978230f29387f6e72e0cb6bc4f0e0f1426c
Jesus <tecnibus7@us.es>
2018-12-07 10:22:40 +0100

    Actualizando script hora

--- config/ajustahora.sh
+++ config/ajustahora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if /bin/netcat -z google.com 80; then echo “Iniciando procedimiento de ajuste horario”; else exit 2; fi
+if netcat -z google.com 80; then echo “Iniciando procedimiento de ajuste horario”; else exit 2; fi
 /sbin/hwclock --systohc
 /bin/date | /usr/bin/tee -a /home/bibliotecario/hora.log
 /sbin/hwclock -r | tee -a /home/bibliotecario/hora.log
