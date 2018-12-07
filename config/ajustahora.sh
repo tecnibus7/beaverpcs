5a5cc65676b6ef9cbfd3d6a49f0323348c6450f6
Jesus <tecnibus7@us.es>
2018-12-07 10:28:37 +0100

    Refactorizando

--- config/ajustahora.sh
+++ config/ajustahora.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if netcat -z google.com 80; then echo “Iniciando procedimiento de ajuste horario”; else exit 2; fi
+if netcat -z google.com 80; then
+  echo “Iniciando procedimiento de ajuste horario”
+else
+  exit 2
+fi
 /sbin/hwclock --systohc
 /bin/date | /usr/bin/tee -a /home/bibliotecario/hora.log
 /sbin/hwclock -r | tee -a /home/bibliotecario/hora.log
